3b547055c1091872b3d15ac012c7d8bbede35569
sanya <sanyagoyal2001@gmail.com>
2021-12-16 20:09:42 +0530

    logo added

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
+import Image from 'next/image'
 function Header() {
     return (
         <div class="sticky flex">
+            <div className="">
+              <Image src="https://raw.githubusercontent.com/sanyagoyal2000/myntra-clone/main/components/images/logo.jpeg" height={500} width={500}/>
+            </div>
             <div className=""></div>
-            <div className=""></div>
+            <div className=""></div> 
             <div className=""></div> 
         </div>
     )
