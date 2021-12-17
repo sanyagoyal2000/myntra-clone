4de62b520009f460af8b8bdf2a8c32ea6c1dfbc2
sanya <sanyagoyal2001@gmail.com>
2021-12-17 09:30:38 +0530

    new logo

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     return (
         <div class="sticky flex">
             <div className="">
-              <Image src="https://raw.githubusercontent.com/sanyagoyal2000/myntra-clone/main/components/images/logo.jpeg" height={500} width={500}/>
+              <Image src="https://raw.githubusercontent.com/sanyagoyal2000/myntra-clone/main/components/images/logo.jpeg" layout="fill" objectFit="contain" objectPosition="left"/>
             </div>
             <div className=""></div>
             <div className=""></div> 
@@ -13,3 +13,4 @@ function Header() {
 }
 
 export default Header
+
